8dd6c057cd9db4bb38ca759735fa266e9e587f87
Sandhya Srinivas <sandhyasrinivas0221@gmail.com>
2021-08-09 11:45:13 +0530

    wileyProject

---
+++ project/Model/Entity4.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Entity4 {
+
+    /**
+     * Default constructor
+     */
+    public Entity4() {
+    }
+
+}
---
+++ project/Model/databaseConnection.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class databaseConnection {
+
+    /**
+     * Default constructor
+     */
+    public databaseConnection() {
+    }
+
+}
