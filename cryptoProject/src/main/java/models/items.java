5efae2163b40a662032d11166a32ef0f1c21ce32
SINGAMPALLI SANDHYA RANI <47891077+Sandhyasrinivas0221@users.noreply.github.com>
2021-08-09 14:26:20 +0530

    Update items.java

--- cryptoProject/src/main/java/models/items.java
+++ cryptoProject/src/main/java/models/items.java
@@ -2,10 +2,10 @@ package models;
 
 public class items {
 	int id; 
-	String name,code;
+	String name,code,addedAt;
 	
 	
-	public items(int id, String name, String code) {
+	public items(int id, String name, String code,String addedAt) {
 		this.id = id;
 		this.name = name;
 		this.code = code;
