9bd2a27be2b3b38b60e44da41a2416d8178b5f23
SINGAMPALLI SANDHYA RANI <47891077+Sandhyasrinivas0221@users.noreply.github.com>
2021-08-09 14:30:36 +0530

    Update wallets.java

--- cryptoProject/src/main/java/models/wallets.java
+++ cryptoProject/src/main/java/models/wallets.java
@@ -3,7 +3,7 @@ package models;
 public class wallets {
 
 	int id,accountId; 
-	String currency_type,status; 
+	String currencyType,status; 
 	float amount; 
 	
 	public wallets(int id, int accountId, String currency_type, String status, float amount) {
