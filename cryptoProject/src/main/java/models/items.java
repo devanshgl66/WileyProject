a4bb3a5785535016e200b78ac00644988e24223b
devanshgl66 <devanshgl55@gmail.com>
2021-08-09 16:13:55 +0530

    nothing

--- cryptoProject/src/main/java/models/items.java
+++ cryptoProject/src/main/java/models/items.java
@@ -2,14 +2,8 @@ package models;
 
 public class items extends Modal{
 	int id; 
-	String name,code,addedAt;
-	
-<<<<<<< Updated upstream
-	
+	String name,code,addedAt;	
 	public items(int id, String name, String code,String addedAt) {
-=======
-	public items(int id, String name, String code) {
->>>>>>> Stashed changes
 		this.id = id;
 		this.name = name;
 		this.code = code;
