a188ccd87233d26fad13807917b9018b916a158f
SINGAMPALLI SANDHYA RANI <47891077+Sandhyasrinivas0221@users.noreply.github.com>
2021-08-09 14:29:15 +0530

    Update walletTransactions.java

--- cryptoProject/src/main/java/models/walletTransactions.java
+++ cryptoProject/src/main/java/models/walletTransactions.java
@@ -2,12 +2,12 @@ package models;
 
 public class walletTransactions {
 	
-	String type,status,mode;
+	String type,status,mode,createdAt;
 	int walletId;
 	float amount; 
 	
 	
-	public walletTransactions(String type, String status, String mode, int walletId, float amount) {
+	public walletTransactions(String type, String status, String mode, int walletId, float amount,String createdAt) {
 		this.type = type;
 		this.status = status;
 		this.mode = mode;
