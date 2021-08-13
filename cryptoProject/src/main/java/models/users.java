f4a0936147c02eecfe1b7370b4489ed8167f15d3
jatin990 <jatinabrol288@gmail.com>
2021-08-13 16:41:46 +0530

    changed users model

--- cryptoProject/src/main/java/models/users.java
+++ cryptoProject/src/main/java/models/users.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 
 public class users extends Model {
 	int id;
-	public String name, email, password, account_no, pan_number, phone_number,created_at;
+	public String name, email, password, account_no, pan_number, phone_number, created_at;
 
 	public String table = "users";
-	public String[] fillable = { "phone_number", "name", "email", "password", "account_no",
-			"pan_number" };
 
-	public users(String phoneNumber, String name, String email, String password, String accountNo, String panNumber,String createdAt) {
-		//setting table name and field name 
-		super("users",new String[] {"phone_number", "name", "email", "password", "account_no",
-			"pan_number" });
+	public users() {
+		super("users", new String[] { "phone_number", "name", "email", "password", "account_no", "pan_number" });
+	}
+
+	public users(String phoneNumber, String name, String email, String password, String accountNo, String panNumber,
+			String createdAt) {
+		// setting table name and field name
+		this();
 		this.phone_number = phoneNumber;
 		this.name = name;
 		this.email = email;
@@ -22,14 +24,12 @@ public class users extends Model {
 		this.pan_number = panNumber;
 		this.created_at = createdAt;
 	}
+
 	@Override
 	public String toString() {
 		return "users [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", account_no="
 				+ account_no + ", pan_number=" + pan_number + ", phone_number=" + phone_number + ", created_at="
 				+ created_at + ", table=" + table + ", fillable=" + Arrays.toString(fillable) + "]";
-	}
-
-	public users() {
 	}
 
 }
